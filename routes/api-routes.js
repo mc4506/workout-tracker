0e7cc23cbaf1ba61bdb8ebed0755ea71e8e53eb9
mc4506 <mike4506@gmail.com>
2020-09-26 22:11:18 -0400

    completed stats get route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../models/index');
 
 router.get('/api/workouts', (req, res) => {
+    // find all workout records
     db.Workout.find({}, (err, workouts) => {
         if(err) {
             res.status(400).send(err);
@@ -13,10 +14,18 @@ router.get('/api/workouts', (req, res) => {
 });
 
 router.get('/api/workouts/range', (req, res) => {
-
+    // find all workout records
+    db.Workout.find({}, (err, workouts) => {
+        if(err) {
+            res.status(400).send(err);
+        } else {
+            res.json(workouts);
+        }
+    });
 });
 
 router.put('/api/workouts/:id', (req, res) => {
+    // find workout record. and push new exercise object into a new exercise array. Update record with new exercise array.
     db.Workout.findOne({_id: req.params.id}, (err, workout) => {
         if(err) {
             res.status(400).send(err);
@@ -36,6 +45,7 @@ router.put('/api/workouts/:id', (req, res) => {
 });
 
 router.post('/api/workouts', (req, res) => {
+    // create a new workout record with current Date object and an empty exercise object. Send back record with id.
     const day = new Date();
     db.Workout.create( {day: day, exercises: []}, (err, workouts) => {
         if(err) {
